678551c97fc1635a081db2ec328faf2df25e9688
Nicky <nickysauceofficial@gmail.com>
2023-04-04 01:26:33 +0530

    basic types in typescript

---
+++ 01-starting-project/basics.ts
@@ -0,0 +1,42 @@
+let age: number;
+
+age = 10;
+age = 100;
+// age = "20"; //error
+
+let userName: string = "Zuko";
+
+let isEligible: boolean = true;
+
+// non-primitive data types
+// array of strings
+
+let skills: string[];
+skills = ["JS", "CSS"];
+
+let data;
+data = {
+  id: "IDX87",
+  createdAt: "Monday",
+  expiresIn: "20hrs",
+};
+
+// object type definition
+let evolve: {
+  current: number;
+  target: number;
+  split: string;
+};
+
+evolve = {
+  current: 78,
+  target: 74,
+  split: "Push Pull Legs",
+};
+
+// array of objects
+let massData: {
+  current: number;
+  target: number;
+  split: string;
+}[];
---
+++ 01-starting-project/typescript.ts
@@ -0,0 +1,7 @@
+function addTwo(a: number, b: number) {
+  return a + b;
+}
+
+const res = addTwo(2, 50);
+
+console.log(res);
