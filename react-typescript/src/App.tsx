9d73a2276e7a36294d34c8e0500fd0bd2d9b931f
Nicky <nickysauceofficial@gmail.com>
2023-04-07 15:57:05 +0530

    typescript typing functions, explicit return types

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -3,17 +3,36 @@ import Todos from "./components/Todos";
 import ToDo from "./models/todo";
 
 function App() {
+  let startDate: Date = new Date();
+  console.log(startDate);
   const todos = [
     new ToDo("DevOps", 1),
     new ToDo("React Native", 2),
     new ToDo("Jenkins", 3),
   ];
+  const num = "literal type";
+
+  function addNumbers(a: number, b: number): number {
+    return a + b;
+  }
   return (
     <div className="App">
-      <h2>TypeScript 🚀</h2>
+      <h2>TypeScript 🚀 {num}</h2>
+      <p>{startDate.getFullYear()}</p>
+      <p>{addNumbers(10, 90)}</p>
       <Todos items={todos} />
     </div>
   );
 }
 
 export default App;
+
+console.log(1);
+Promise.resolve(3)
+  .then((res) => {
+    console.log(res);
+    return 4;
+  })
+  .then((res) => console.log(res));
+setTimeout(() => console.log(2), 0);
+console.log(5);
--- react-typescript/src/components/Todos.tsx
+++ react-typescript/src/components/Todos.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Todo from "../models/todo";
 
+// here we are explicitly setting the generic type of React.FC
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul style={{ width: "10rem" }}>
