6d8d5e8040d5f0758120ed0d01dead5ecd97348c
Nicky <nickysauceofficial@gmail.com>
2023-04-14 09:49:35 +0530

    JSON type annotation

--- ts-basics/01-starting-project/basics.ts
+++ ts-basics/01-starting-project/basics.ts
@@ -146,3 +146,42 @@ let myCar: [number, string, string] = [2024, "Audi", "Q7"];
 // type checking
 
 // Interface
+
+interface UserInfo {
+  name: string;
+  email: string;
+}
+function printUserInfo(info: UserInfo) {
+  console.log(info.name);
+}
+
+// Defining valid types of a JSON, JSON cant be a function remember.
+// @errors: 2578
+type Primitive = string | boolean | number | null;
+type JSONObject = { [key: string]: JSONValue };
+type JSONArray = JSONValue[];
+type JSONValue = Primitive | JSONArray | JSONObject;
+
+////// DO NOT EDIT ANY CODE BELOW THIS LINE //////
+function isJSON(arg: JSONValue) {}
+
+// POSITIVE test cases (must pass)
+isJSON("hello");
+isJSON([4, 8, 15, 16, 23, 42]);
+isJSON({ greeting: "hello" });
+isJSON(false);
+isJSON(true);
+isJSON(null);
+isJSON({ a: { b: [2, 3, "foo"] } });
+
+// NEGATIVE test cases (must fail)
+// @ts-expect-error
+isJSON(() => "");
+// @ts-expect-error
+isJSON(class {});
+// @ts-expect-error
+isJSON(undefined);
+// @ts-expect-error
+isJSON(new BigInt(143));
+// @ts-expect-error
+isJSON(isJSON);
